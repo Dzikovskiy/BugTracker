0326686c6b32212c351749cf67c6e56bf9b2bda7
Dzikovskiy <43319439+Dzikovskiy@users.noreply.github.com>
2019-11-30 15:37:32 +0300

    Pane test

--- src/app/controllers/BoardController.java
+++ src/app/controllers/BoardController.java
@@ -1,12 +1,19 @@
 package app.controllers;
 import java.net.URL;
 import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
 public class BoardController {
+
+    @FXML
+    private ScrollPane scrollPane;
 
     @FXML
     private ResourceBundle resources;
@@ -15,20 +22,14 @@ public class BoardController {
     private URL location;
 
     @FXML
-    private Button loginSignUpButton;
-
-    @FXML
-    private PasswordField password_field;
-
-    @FXML
-    private TextField login_field;
-
-    @FXML
-    private Button loginSignInButton;
-
-    @FXML
     void initialize() {
 
+        Rectangle rectangle = new Rectangle(30,40, Color.web("#ed4b00"));
+        for(int i = 0;i<10;i++) {
+            Group
+            scrollPane.setContent();
+            scrollPane.setContent(rectangle);
+        }
 
     }
 }
